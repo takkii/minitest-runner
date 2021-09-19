1dfb9a3b3bb4bb56c0625dcf5550ee051ca9480f
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-09-19 10:57:52 +0900

    Update.

---
+++ req/require.rb
@@ -0,0 +1,6 @@
+# --------------------------------------
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/reporters'
+# --------------------------------------
